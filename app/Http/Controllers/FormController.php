73ad3c0e7183665292f6a3a6231726affef04f3d
scopez-jelmer <scopezjelmer@gmail.com>
2024-01-02 16:59:51 +0100

    first setup demo env

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,19 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use src\ValidationHelper;
 
 class FormController extends Controller
 {
     public function store(Request $request)
     {
+
         $validated = $request->validate([
-            'name' => 'required|max:255',
-            // 'iets' => 'required',
+            'name' => ValidationHelper::validateString(true),
         ]);
-        // dd($validated);
         if ($validated) {
             return redirect('/validation-works');
         }
-        die();
     }
 }
